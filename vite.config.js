b81763630ffbd1d4f20c81c0bb5065ffb6c21e99
akhilpa555 <46228116+akhilpa555@users.noreply.github.com>
2026-01-18 12:44:55 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  build: {
+    outDir: 'docs',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+})
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
